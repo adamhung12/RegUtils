a658c565912ecb263190e7232c4bf3d648dec3b8
Xeth Hung <xcwhung@ogcio.gov.hk>
2020-09-24 18:29:47 +0800

    1. fix incorrect project name 2. add test 3. add stream support

--- src/main/sources/me/xethh/utils/ReqUtils/Reg.java
+++ src/main/sources/me/xethh/utils/ReqUtils/Reg.java
@@ -1,28 +1,48 @@
 package me.xethh.utils.ReqUtils;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import me.xethh.utils.functionalPacks.JStream;
+import me.xethh.utils.functionalPacks.tuples.Tuple2;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
+/**
+ * {@link Reg} is targeted to reduce the redundant coding for using java.util.regex.*
+ */
 public class Reg {
-    private Pattern pattern;
+    private final Pattern pattern;
+
+    /**
+     * Constructor only accessible from static method {@link #of(String)}
+     * @param reg the regular expression string
+     */
     private Reg(String reg){
         pattern = Pattern.compile(reg);
     }
 
+    /**
+     * Match the string with regex pattern and return {@link Matching} object
+     * @param str string to match
+     * @return {@link Matching}
+     */
     public Matching matching(String str){
         return new Matching(pattern.matcher(str), this);
     }
 
+    /**
+     * {@link Matching} object provide functionality to as match whole word ({@link #ifMatch(Consumer)}, {@link #ifMatchAndReturn(Function)}, {@link #ifMatchAndReturns(Function[])})
+     * or find matched segment ({@link #loopMatched()}, {@link #loopMatched(int)}, {@link #loopMatchedStream()}, {@link #loopMatchedStream(int), {@link #loopMatchedZippedStream()}})
+     */
     public static class Matching{
-        private Matcher matcher;
-        private Reg reg;
+        private final Matcher matcher;
+        private final Reg reg;
         private Matching(Matcher matcher, Reg reg){
             this.matcher = matcher;
             this.reg = reg;
@@ -34,6 +54,10 @@ public class Reg {
 
         public Reg reg() {
             return reg;
+        }
+
+        public boolean matches(){
+            return matcher.matches();
         }
 
         public Matching ifMatch(Consumer<Matcher> consumer){
@@ -48,15 +72,54 @@ public class Reg {
             }
             return Optional.empty();
         }
-        public List<Optional<String>> ifMatchAndReturns(Function<Matcher, String> ...operation){
+
+        public Iterator<String> loopMatched(){
+            return loopMatched(0);
+        }
+
+        public Iterator<String> loopMatched(int index){
+            return new Iterator<>() {
+                final AtomicBoolean first = new AtomicBoolean(true);
+                @Override
+                public boolean hasNext() {
+                    if(first.get()){
+                        first.compareAndSet(true, false);
+                        return matcher.find(index);
+                    }
+                    else{
+                        return matcher.find();
+                    }
+                }
+
+                @Override
+                public String next() {
+                    return matcher.group();
+                }
+            };
+        }
+
+        @SafeVarargs
+        public final List<Optional<String>> ifMatchAndReturns(Function<Matcher, String>... operation){
             if(matcher.matches()){
                 return Arrays.stream(operation).map(it->Optional.ofNullable(it.apply(matcher))).collect(Collectors.toList());
             }
             return new ArrayList<>();
         }
+        public final Stream<String> loopMatchedStream(int index){
+            Spliterator<String> spliterator = Spliterators.spliteratorUnknownSize(loopMatched(), index);
+            return StreamSupport.stream(spliterator, false);
+        }
+        public final Stream<String> loopMatchedStream(){
+            return loopMatchedStream(0);
+        }
+        public final Stream<Tuple2<Integer, String>> loopMatchedZippedStream(){
+            return JStream.zipWithIndex(loopMatched());
+        }
+
     }
 
     public static Reg of(String reg){
         return new Reg(reg);
     }
+
 }
--- src/test/sources/me/xethh/utils/ReqUtils/AppTest.java
+++ src/test/sources/me/xethh/utils/ReqUtils/AppTest.java
@@ -1,9 +1,16 @@
 package me.xethh.utils.ReqUtils;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit test for simple App.
@@ -16,12 +23,145 @@ public class AppTest {
     public void shouldAnswerWithTrue() {
         Reg.of("^(\\d+)$")
                 .matching("12345")
-                .ifMatch(m -> {
-                    assertEquals("12345", m.group());
-                })
-                .ifMatch(m -> {
-                    assertEquals("12345", m.group(1));
-                })
+                .ifMatch(m -> assertEquals("12345", m.group()))
+                .ifMatch(m -> assertEquals("12345", m.group(1)))
         ;
     }
+
+    @Test
+    public void test1() {
+        Optional<String> rs = Reg.of("^(\\d+)$")
+                .matching("12345")
+                .ifMatchAndReturn(Matcher::group);
+        rs.ifPresentOrElse(str -> assertEquals("12345", str), Assert::fail);
+    }
+
+    @Test
+    public void test() {
+        List<Optional<String>> rs = Reg
+                .of("^(\\d+),(\\d+)$")
+                .matching("12345,1234")
+                .ifMatchAndReturns(
+                        m -> m.group(1),
+                        m -> m.group(2)
+                );
+
+        rs.get(0).ifPresentOrElse(str -> assertEquals("12345", str), Assert::fail);
+        rs.get(1).ifPresentOrElse(str -> assertEquals("1234", str), Assert::fail);
+    }
+
+    @Test
+    public void test3() {
+        String str = "122,444,6673,00,12,1";
+        Reg.Matching matching = Reg
+                .of("\\d+")
+                .matching(str);
+
+        Iterator<String> it = matching.loopMatched();
+        it.hasNext();
+        assertEquals("122", it.next());
+        it.hasNext();
+        assertEquals("444", it.next());
+        it.hasNext();
+        assertEquals("6673", it.next());
+        it.hasNext();
+        assertEquals("00", it.next());
+        it.hasNext();
+        assertEquals("12", it.next());
+        it.hasNext();
+        assertEquals("1", it.next());
+
+        it = matching.loopMatched(0);
+        it.hasNext();
+        assertEquals("122", it.next());
+        it.hasNext();
+        assertEquals("444", it.next());
+        it.hasNext();
+        assertEquals("6673", it.next());
+        it.hasNext();
+        assertEquals("00", it.next());
+        it.hasNext();
+        assertEquals("12", it.next());
+        it.hasNext();
+        assertEquals("1", it.next());
+
+        it = matching.loopMatched(1);
+        it.hasNext();
+        assertEquals("22", it.next());
+        it.hasNext();
+        assertEquals("444", it.next());
+        it.hasNext();
+        assertEquals("6673", it.next());
+        it.hasNext();
+        assertEquals("00", it.next());
+        it.hasNext();
+        assertEquals("12", it.next());
+        it.hasNext();
+        assertEquals("1", it.next());
+
+        it = matching.loopMatched(2);
+        it.hasNext();
+        assertEquals("2", it.next());
+        it.hasNext();
+        assertEquals("444", it.next());
+        it.hasNext();
+        assertEquals("6673", it.next());
+        it.hasNext();
+        assertEquals("00", it.next());
+        it.hasNext();
+        assertEquals("12", it.next());
+        it.hasNext();
+        assertEquals("1", it.next());
+
+        it = matching.loopMatched(3);
+        it.hasNext();
+        assertEquals("444", it.next());
+        it.hasNext();
+        assertEquals("6673", it.next());
+        it.hasNext();
+        assertEquals("00", it.next());
+        it.hasNext();
+        assertEquals("12", it.next());
+        it.hasNext();
+        assertEquals("1", it.next());
+
+        it = matching.loopMatched(4);
+        it.hasNext();
+        assertEquals("444", it.next());
+        it.hasNext();
+        assertEquals("6673", it.next());
+        it.hasNext();
+        assertEquals("00", it.next());
+        it.hasNext();
+        assertEquals("12", it.next());
+        it.hasNext();
+        assertEquals("1", it.next());
+
+        assertEquals("122:444:6673:00:12:1", matching.loopMatchedStream().collect(Collectors.joining(":")));
+        matching.loopMatchedZippedStream()
+                .forEach(item -> {
+                    switch (item.v1) {
+                        case 0:
+                            assertEquals("122", item.v2);
+                            break;
+                        case 1:
+                            assertEquals("444", item.v2);
+                            break;
+                        case 2:
+                            assertEquals("6673", item.v2);
+                            break;
+                        case 3:
+                            assertEquals("00", item.v2);
+                            break;
+                        case 4:
+                            assertEquals("12", item.v2);
+                            break;
+                        case 5:
+                            assertEquals("1", item.v2);
+                            break;
+                    }
+                });
+
+    }
+
 }
